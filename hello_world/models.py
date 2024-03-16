65cdddede4b4235e3698c697975ad402d8e848c6
samantha_nisus_johnson <128253624+SamJohn87@users.noreply.github.com>
2024-03-16 14:32:07 -0500

    work with models and data in Django

--- hello_world/models.py
+++ hello_world/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=1000, decimal_places=2)
+    creation_date = models.DateField(auto_now_add=True)
+    category = models.ForeignKey(
+        'Category', #The name of the model
+        on_delete=models.PROTECT
+    )
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
